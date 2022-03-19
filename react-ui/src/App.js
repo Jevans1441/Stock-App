641b199957f5e01d39fb7fdf9d2cda60e0797e61
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2022-03-19 12:00:44 -0400

    react-ui, dependencies

---
+++ react-ui/src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">HELLO WORLD</header>
+    </div>
+  );
+}
+
+export default App;
