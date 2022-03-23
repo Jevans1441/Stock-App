35b23bf3e913b288e540e5c7071e9caaa556376e
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2022-03-23 17:56:35 -0400

    uninstalled redux

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Provider } from "react-redux";
-import store from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
 import "./styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
